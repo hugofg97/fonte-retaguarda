c7e0cf251697c7dca5f4759413785164019c2fd9
hugofg97 <hugo.odev97@gmail.com>
2022-01-18 22:47:35 -0300

    api

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ let headers = new Headers();
 
 console.log(headers);
 const http = axios.create({
-  baseURL: "http://localhost:3000/dev/",
+  baseURL: "https://ll261boi04.execute-api.sa-east-1.amazonaws.com/dev/",
   headers: {
     'Authorization': 'Basic '+Buffer.from('sk_test_Z58AQoXcghQe91kb:').toString('base64'),
     'Content-Type': 'application/json', 
